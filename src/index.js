1b23a1644ef012655d21b242bc86367ed14e82e5
Ilya Averin <37779678+ilya-averin@users.noreply.github.com>
2019-05-07 23:27:05 +0200

    next commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ class SwapiService {
 
 	async getResource(url) {
 		const res = await fetch(`${this._apiBase}${url}`);
-	
+
   	if (!res.ok) {
-			throw new Error(`Could not fetch ${url}` + 
+			throw new Error(`Could not fetch ${url}` +
 				`, received ${res.status}`)
 		}
   	return await res.json();
@@ -15,22 +15,37 @@ class SwapiService {
 
 	async getAllPeople() {
 		const res = await this.getResource(`/people/`);
-			return res.results;	
+			return res.results;
 	}
 
 	getPerson(id) {
 		return this.getResource(`/people/${id}/`);
 	}
+
+	async getAllPlanets() {
+		const res = await this.getResource(`/planets/`);
+			return res.results;
+	}
+
+	getPlanet(id) {
+		return this.getResource(`/planets/${id}/`);
+	}
+
+	async getAllStarships() {
+		const res = await this.getResource(`/starships/`);
+			return res.results;
+	}
+
+	getStarship(id) {
+		return this.getResource(`/starships/${id}/`);
+	}
 }
 
 const swapi = new SwapiService();
 
-swapi.getAllPeople().then((people) => {
-	people.forEach((p) => {
+swapi.getPerson(3).then((p) => {
 		console.log(p.name);
-	})
 });
 
-	
 
-  
+
