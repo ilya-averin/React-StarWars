6d28623fddb66d94ab04e09025d0802a567f4770
Ilya Averin <37779678+ilya-averin@users.noreply.github.com>
2019-05-07 23:14:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+class SwapiService {
+
+	_apiBase = 'https://swapi.co/api';
+
+	async getResource(url) {
+		const res = await fetch(`${this._apiBase}${url}`);
+	
+  	if (!res.ok) {
+			throw new Error(`Could not fetch ${url}` + 
+				`, received ${res.status}`)
+		}
+  	return await res.json();
+	}
+
+	async getAllPeople() {
+		const res = await this.getResource(`/people/`);
+			return res.results;	
+	}
+
+	getPerson(id) {
+		return this.getResource(`/people/${id}/`);
+	}
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+	people.forEach((p) => {
+		console.log(p.name);
+	})
+});
+
+	
+
+  
